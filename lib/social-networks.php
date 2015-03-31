8cdbdbe3c5f7f76e04c8b1307dfe830a6996f2e9
cumanzorx07 <shadow.x07@gmail.com>
2015-03-31 00:13:44 -0600

    Adding first smallset pack with sprite (using CSS3 and complex selector)

--- lib/social-networks.php
+++ lib/social-networks.php
@@ -1,19 +1,4 @@
 <?php
-/*
-	Declare supported social media websites
-
-	By default, the Social Icons Widget provides a variety of popular social media websites. Developers can easily add more social media websites by creating a filter in the active theme's functions.php file like such:
-
-		`function add_new_icons($icon_list) {
-			$icon_list['Full Website Name'] = 'full-website-id';
-		 
-			return $icon_list;
-		}
-		add_filter('social_icon_accounts', 'add_new_icons');`
-
-	The full-website-id should reflect the name of the image you create in each of the icon folder sizes, or in your custom icon directory. It is also used to populate the class field of the icon when the widget displays. The Social Icon Widget looks for .gif, .jpg, .jpeg, and .png in order and returns the first extention it finds.
-*/
-
 	global $smwi_social_accounts;
 	$smwi_social_accounts = array(
 		'500px' => 'fivehundredpx',
--- lib/widget.php
+++ lib/widget.php
@@ -17,7 +17,7 @@ if($smwi_show_title == '') {
 
 $ul_class = ($smwi_labels == 'show') ? 'show-labels ' : '' ;
 $ul_class .= 'icons-'.$smwi_icons;
-echo apply_filters('social_icon_opening_tag', '<ul class="'.$ul_class.'">');
+echo apply_filters('social_icon_opening_tag', '<div class="social-icons-widget"><ul class="'.$ul_class.'">');
  
 foreach($smwi_social_accounts as $smwi_title => $id) :
 	if(!empty($instance[$id]) != '' && $instance[$id] != 'http://') :		
@@ -27,73 +27,28 @@ foreach($smwi_social_accounts as $smwi_title => $id) :
 		$smwi_data['url'] = $instance[$id];
 		
 		//in case of using NONE as the icon set, we will force labels
+		$format = "";
+		$dataToFormat = array();
 		if($smwi_icons != "none")
-		{
-			$smwi_custom_sizes = array('custom_small','custom_medium','custom_large');
-		
-			if (in_array($smwi_icons, $smwi_custom_sizes)) {
-				$size = str_replace("custom_","",$smwi_icons);
-				$smwi_icon_path = STYLESHEETPATH .'/social_icons/'.$size.'/'.$id.'.{gif,jpg,jpeg,png}';
-			}
-			else {
-				$smwi_abs_path = str_replace('lib/', '', plugin_dir_path( __FILE__ ));
-	
-				/*	Fix for Windows/XAMPP where the slash goes the wrong way.
-					Thanks to VictoriousK */
-				$smwi_abs_path = str_replace('\\', '/', $smwi_abs_path);
-				
-				$smwi_icon_path =  $smwi_abs_path . 'icons/'.$smwi_icons.'/'.$id.'.{gif,jpg,jpeg,png}';
-				
-				switch($smwi_icons)
-				{
-					case 'large':
-						$imgsize = 'height="64" width="64"';
-						break;
-					case 'medium':
-						$imgsize = 'height="32" width="32"';
-						break;
-					case 'small':
-						$imgsize = 'height="16" width="16"';
-						break;
-				}
-			}
-			
-			$result = glob( $smwi_icon_path, GLOB_BRACE );
-	
-			if($result) {
-				if (in_array($smwi_icons, $smwi_custom_sizes)) {
-					$smwi_path = explode('themes', $result[0]);
-					$smwi_icon = get_bloginfo('url').'/wp-content/themes'.$smwi_path[1];
-				}
-				else {
-					$smwi_path = explode('plugins', $result[0]);
-					$smwi_icon = plugins_url().''.$smwi_path[1];
-				}
-			}
-			elseif( $smwi_labels != 'show' && $smwi_icons != 'small' ) {
-				$smwi_icon = plugins_url().'/social-media-icons-widget/icons/'.$smwi_icons.'/_unknown.jpg';
-			}
-			else {
-				$smwi_icon = '';
-			}
-			if ( $smwi_icon ) { $smwi_data['image'] = '<img class="site-icon" src="'.$smwi_icon.'" alt="'.$smwi_title.'" title="'.$smwi_title.'" '.$imgsize.' />'; }
-			else { $smwi_data['image'] = ''; }
+		{			
+			$dataToFormat['classCSS'] = $smwi_icons."_".$id;
+			$dataToFormat['url'] = $instance[$id];	
+			$dataToFormat['label'] = ($smwi_labels == 'show') ? '<span class="site-label">'.$smwi_title.'</span>' : '';			
+			$format = '<li class="%1$s"><a href="%2$s" target="_blank"><span class="site-icon"></span>%3$s</a></li>';			
 		}
 		else
 		{
-				$smwi_data['image'] = $smwi_title;				
+			$dataToFormat['classCSS'] = "labelonly";
+			$dataToFormat['url'] = $instance[$id];	
+			$dataToFormat['label'] = '<span class="site-label">'.$smwi_title.'</span>';
+			$format = '<li class="%1$s"><a href="%2$s" target="_blank">%3$s</a></li>';
 		}		
-		
-		if($smwi_labels != 'show') { $smwi_data['title'] = ''; }
-		else { $smwi_data['title'] = '<span class="site-label">'.$smwi_title.'</span>'; }
-	
-		$format = '<li class="%1$s"><a href="%2$s" target="_blank">%3$s%4$s</a></li>';
 		$smwi_icon_output = apply_filters('social_icon_output', $format);
-		echo vsprintf($smwi_icon_output, $smwi_data);
+		echo vsprintf($smwi_icon_output, $dataToFormat);
 
 	endif; 
 endforeach; 
 
-echo apply_filters('social_icon_closing_tag', '</ul>'); 
+echo apply_filters('social_icon_closing_tag', '</ul></div>'); 
 echo $after_widget;
 ?>
